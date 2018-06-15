7164244e53ee77315011733771f2502cae4f92ee
raizul8 <bogarazvan2@gmail.com>
2018-06-15 10:36:24 +0300

    keep_projects: vineri 15 iunie 2018, 10:36:24 +0300

--- goKeep/gas/gas_05_2017/startGas.sh
+++ goKeep/gas/gas_05_2017/startGas.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd /home/raz/IdeaProjects1/goKeep/gas/gas_05_2017/src/gasHttp
+# cd /home/raz/IdeaProjects1/goKeep/gas/gas_05_2017/src/gasHttp
+cd /home/raz/CodeProjects/keep_projects/goKeep/gas/gas_05_2017/src/gasHttp
 
 ./gasHttp false & /opt/google/chrome/google-chrome http://localhost:8000/static/gasWeb/gas.html https://myline-eon.ro/login;
 
