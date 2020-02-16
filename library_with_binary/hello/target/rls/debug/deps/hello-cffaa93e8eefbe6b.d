546837bdc17836d3ce64a5b3a5f8d29ae4845c54
raizul8 <bogarazvan2@gmail.com>
2020-02-16 13:04:19 +0200

    keep_projects: Sun Feb 16 13:04:19 EET 2020

--- library_with_binary/hello/target/rls/debug/deps/hello-cffaa93e8eefbe6b.d
+++
@@ -1,5 +0,0 @@
-/home/raz/code_projects/keep_projects/library_with_binary/hello/target/rls/debug/deps/hello-cffaa93e8eefbe6b.rmeta: src/lib.rs
-
-/home/raz/code_projects/keep_projects/library_with_binary/hello/target/rls/debug/deps/hello-cffaa93e8eefbe6b.d: src/lib.rs
-
-src/lib.rs:
--- library_with_binary/hello/target/rls/debug/deps/main-1f355a4ac25ae8ce.d
+++
@@ -1,5 +0,0 @@
-/home/raz/code_projects/keep_projects/library_with_binary/hello/target/rls/debug/deps/main-1f355a4ac25ae8ce.rmeta: src/bin/main.rs
-
-/home/raz/code_projects/keep_projects/library_with_binary/hello/target/rls/debug/deps/main-1f355a4ac25ae8ce.d: src/bin/main.rs
-
-src/bin/main.rs:
